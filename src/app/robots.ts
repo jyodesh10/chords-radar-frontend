703ac5b2a448c5459e811423d02b3ced3dfd4c21
jyodesh10 <jyodeshshakya@gmail.com>
2026-03-28 21:41:36 +0100

    updated robot.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,10 +4,16 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = 'https://chords-radar-nepal.vercel.app';
 
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: 'Mediapartners-Google',
+        allow: ['/app-ads.txt'],
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
